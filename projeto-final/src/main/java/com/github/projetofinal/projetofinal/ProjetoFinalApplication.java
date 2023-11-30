193818e13470cbb21c8e2772d7b5d38e92aa0ee6
camila2601 <99697225+camila2601@users.noreply.github.com>
2023-11-30 20:01:55 -0300

    Deletando

--- projeto-final/src/main/java/com/github/projetofinal/projetofinal/ProjetoFinalApplication.java
+++ projeto-final/src/main/java/com/github/projetofinal/projetofinal/ProjetoFinalApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProjetoFinalApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoFinalApplication.class, args);
 	}
